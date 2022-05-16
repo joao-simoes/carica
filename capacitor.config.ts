8ce4107b2b6f8cfc03625fc636ee9226217c73ad
joao-simoes <joaopsimoes0@gmail.com>
2022-05-16 19:28:57 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carica',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
